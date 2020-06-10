6ea25d1e854afcbe48b7099aa65c3f9236018323
Robert Arteaga <daspanisharmada@gmail.com>
2020-06-10 06:40:22 -0700

    basic theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
-Vue.use(Vuetify);
-
-export default new Vuetify({
+Vue.use(Vuetify, {
+	iconfont: 'md',
+	theme: {
+		primary: '#9652ff',
+	},
 });
+
+export default new Vuetify({});
