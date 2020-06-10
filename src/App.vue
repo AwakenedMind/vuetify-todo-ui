208aa45eb39ccef50981dbc193000ba9b02bbc37
Robert Arteaga <daspanisharmada@gmail.com>
2020-06-10 05:50:06 -0700

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <v-app>
+      <Navbar />
+      <v-content>
+        <router-link to="/">Home</router-link>|
+        <router-link to="/about">About</router-link>
+        <router-view />
+      </v-content>
+    </v-app>
   </div>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar";
+
+export default {
+  name: "App",
+  components: { Navbar },
+  data() {
+    return {};
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
