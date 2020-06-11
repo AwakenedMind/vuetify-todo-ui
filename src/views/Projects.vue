4e0beb745b2ed914cd1b682115b9224009986d95
Robert Arteaga <daspanisharmada@gmail.com>
2020-06-11 03:46:02 -0700

    added project cards

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,11 +1,72 @@
 <template>
   <div class="projects">
-    <h1 class="subtitle-1 grey--text">Dashboard</h1>
+    <v-container class="blue--text my-5">
+      <v-expansion-panels>
+        <v-expansion-panel class="black--text" v-for="project in projects" :key="project.title">
+          <v-expansion-panel-header>{{project.title}}</v-expansion-panel-header>
+
+          <v-expansion-panel-content class="px-4">
+            <div class="font-weight-bold">{{project.due}}</div>
+
+            <div>{{project.content}}</div>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    myProjects() {
+      return this.projects.filter(project => {
+        return project.person === "The Net Ninja";
+      });
+    }
+  },
+  data() {
+    return {
+      projects: [
+        {
+          title: "Design a new website",
+          person: "The Net Ninja",
+          due: "1st Jan 2019",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+
+        {
+          title: "Code up the homepage",
+          person: "Chun Li",
+          due: "10th Jan 2019",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+
+        {
+          title: "Design video thumbnails",
+          person: "Ryu",
+          due: "20th Dec 2018",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+
+        {
+          title: "Create a community forum",
+          person: "Gouken",
+          due: "20th Oct 2018",
+          status: "overdue",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
